a8067c38da2230c1b447a10f27d0b6e592264703
DeraBola <96840776+DeraBola@users.noreply.github.com>
2022-08-18 15:42:11 +0100

    commit

--- .history/src/container/Header/Header_20220818153328.jsx
+++ .history/src/container/Header/Header_20220818153328.jsx
@@ -9,11 +9,9 @@ const Header = () => (
       <SubHeading title="Chase the new Flavour" />
       <h1 className="app__header-h1">The key to Fine dining</h1>
       <p className="p__opensans" style={{margin:'2rem 0'}} >Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-    <button type="button"  className="custom__button">Explore-Menu</button>
+    <button type></button>
     </div>
-    <div className="app__wrapper_img">
-      <img src={images.welcome} alt="header-img "/>
-    </div>
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
--- .history/src/container/Header/Header_20220818153543.jsx
+++ .history/src/container/Header/Header_20220818153543.jsx
@@ -11,9 +11,7 @@ const Header = () => (
       <p className="p__opensans" style={{margin:'2rem 0'}} >Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
     <button type="button"  className="custom__button">Explore-Menu</button>
     </div>
-    <div className="app__wrapper_img">
-      <img src={images.welcome} alt="header-img "/>
-    </div>
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
