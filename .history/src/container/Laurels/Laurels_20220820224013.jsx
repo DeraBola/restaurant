74270b6e21d31605a138b337b3d3552f9ce27bda
DeraBola <96840776+DeraBola@users.noreply.github.com>
2022-08-20 22:51:12 +0100

    commit

---
+++ .history/src/container/Laurels/Laurels_20220820224013.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SubHeading, MenuItem } from "../../components";
+import { images, data } from "../../constants";
+import './Laurels.css';
+
+const Laurels = () => (
+  <div>
+    Laurels
+  </div>
+);
+
+export default Laurels;
---
+++ .history/src/container/Laurels/Laurels_20220820224442.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SubHeading } from "../../components";
+import { images, data } from "../../constants";
+import './Laurels.css';
+
+const Laurels = () => (
+  <div className="app__bg app__padding">
+    Laurels
+  </div>
+);
+
+export default Laurels;
