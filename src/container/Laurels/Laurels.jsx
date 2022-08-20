984baefac450233d3957731b9ef34816d6f0e34b
DeraBola <96840776+DeraBola@users.noreply.github.com>
2022-08-20 22:49:47 +0100

    commit

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { SubHeading } from "../../components";
+import { images, data } from "../../constants";
 import './Laurels.css';
 
 const Laurels = () => (
-  <div>
+  <div className="app__bg app__wrapper section__padding "id="awards">
+    
     Laurels
   </div>
 );
